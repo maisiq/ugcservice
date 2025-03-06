6c8b726b65b6aedaf08109b1a68df546ebbada2c
Vladislav Vasilev <vyruscd@gmail.com>
2025-03-06 12:48:48 +0400

    Add sentry sdk

--- api/src/main.py
+++ api/src/main.py
@@ -1,8 +1,24 @@
+from os import getenv
+
 from fastapi import FastAPI
+import sentry_sdk
 
 from .movies.router import router as main_router
 from .analytics.router import router as analytics_router
 
+
+SENTRY_DSN = getenv('SENTRY_DSN')
+
+
+if SENTRY_DSN:
+    sentry_sdk.init(
+        dsn=SENTRY_DSN,
+        send_default_pii=True,
+        traces_sample_rate=1.0,
+        _experiments={
+            "continuous_profiling_auto_start": True,
+        },
+    )
 
 app = FastAPI(
     debug=True,
