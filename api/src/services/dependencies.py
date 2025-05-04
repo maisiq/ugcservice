6e604302fe563294b2ab845c881c037d7d52d35f
Vladislav Vasilev <vyruscd@gmail.com>
2025-05-05 00:04:35 +0400

    Add FastAPI UserMovieService dependency

---
+++ api/src/services/dependencies.py
@@ -0,0 +1,16 @@
+from fastapi import Depends
+from motor.motor_asyncio import AsyncIOMotorClientSession
+
+from src.core.uow import MongoUOW
+from src.db.dependencies import get_session
+from src.db.repositories.mongodb.movie import MongoMovieRepository
+from src.db.repositories.mongodb.user import MongoUserRepository
+from src.services.user_movie import MongoUserMovieService
+
+
+async def user_movie_service(session: AsyncIOMotorClientSession = Depends(get_session, use_cache=False)):
+    uow = MongoUOW(session)
+    user_repo = MongoUserRepository(session)
+    movie_repo = MongoMovieRepository(session)
+
+    yield MongoUserMovieService(uow, user_repo, movie_repo)
