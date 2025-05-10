9ad48f2c12fe5e2b4d835ffd22cdf9ff8c89faa4
Vladislav Vasilev <vyruscd@gmail.com>
2025-05-10 19:54:13 +0400

    Update repositories to fit UserMovieService

--- api/src/db/repositories/mongodb/movie.py
+++ api/src/db/repositories/mongodb/movie.py
@@ -6,24 +6,28 @@ from src.db.config import MONGO_DB, MongoCollections
 class MongoMovieRepository:
     def __init__(self, session: AsyncIOMotorClientSession):
         self._session = session
-        self._coll = self._session.client[MONGO_DB][MongoCollections.movies] # ADD CONSTANT VALUE TO CONFIG
+        self._coll = self._session.client[MONGO_DB][MongoCollections.movies]
 
-    async def reviews(self, movie_id): 
+    async def reviews(self, movie_id):
         data = await self._coll.find_one(
-            {'_id': movie_id}, 
+            {'_id': movie_id},
             {'_id': 0, 'reviews': 1},
             session=self._session,
         )
         return data.get('reviews') if data is not None else []
 
-    async def rating(self, movie_id) -> dict[str, str | None]: 
+    async def rating(self, movie_id) -> dict[str, str | None]:
         data = await self._coll.find_one(
             {'_id': movie_id},
-            {'_id': 0, 'rating': {'$sum': '$ratings.value'}},
+            {'_id': 0, 'rating': {'$avg': '$ratings.value'}},
             session=self._session,
         )
-        return data if data is not None else {'rating': None}
-    
+        if data is not None:
+            resp = {'rating': round(data['rating'], 1)}
+        else:
+            resp = {'rating': None}
+        return resp
+
     async def add_review(self, user_id, movie_id, review):
         result = await self._coll.find_one(
             {'_id': movie_id, 'reviews.user_id': user_id},
@@ -32,7 +36,7 @@ class MongoMovieRepository:
 
         if result:
             raise Exception(' This user already has review on movie')
-        
+
         result = await self._coll.update_one(
             {'_id': movie_id},
             {'$push': {
@@ -59,4 +63,19 @@ class MongoMovieRepository:
             {'$pull': {'reviews': {'user_id': user_id}}},
             session=self._session,
         )
+        return result.acknowledged
+
+    async def rate(self, user_id, movie_id, value):
+        result = await self._coll.update_one(
+            {'_id': movie_id, 'ratings.user_id': user_id},
+            {'$set': {'ratings.$.value': value}},
+        )
+
+        if result.matched_count == 0:
+            result = await self._coll.update_one(
+                {'_id': movie_id},
+                {'$push': {'ratings': {'user_id': user_id, 'value': value}}},
+                session=self._session,
+                upsert=True,
+            )
         return result.acknowledged
