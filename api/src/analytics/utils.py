a6af09a9eb7377ace80b5129bc03f4a25eaf8267
Vladislav Vasilev <vyruscd@gmail.com>
2025-05-19 12:36:14 +0400

    Update utils to use new config

--- api/src/analytics/utils.py
+++ api/src/analytics/utils.py
@@ -1,11 +1,11 @@
-from os import getenv
-
 from aiokafka import AIOKafkaProducer
 
+from .config import kafka_config
 
-async def send_one(topic, message):
+
+async def send_one(topic: str, message: bytes):
     producer = AIOKafkaProducer(
-        bootstrap_servers=[getenv('KAFKA_SERVER0'), getenv('KAFKA_SERVER1'), getenv('KAFKA_SERVER2')],
+        bootstrap_servers=kafka_config.BOOTSTRAP_SERVERS,
     )
     # Get cluster layout and initial topic/partition leadership information
     await producer.start()
