b52f3554194417e52acd86db24c8460a83f48f18
Vladislav Vasilev <vyruscd@gmail.com>
2025-05-20 13:29:40 +0400

    feat(ETL): Add config file
    Now ETL consumer uses new config

---
+++ ETL/config.py
@@ -0,0 +1,52 @@
+from enum import Enum
+
+from pydantic import field_validator
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Topics(Enum):
+    analytics = 'analytics'
+
+
+class KafkaConfig(BaseSettings):
+    CONSUMER_GROUP_ID: str = 'analytics'
+    BOOTSTRAP_SERVERS: list[str]
+
+    SERVER0: str
+    SERVER1: str
+    SERVER2: str
+
+    @field_validator("BOOTSTRAP_SERVERS", mode="before")
+    def assemble_bootstrap_servers(cls, v, values):
+        if v is not None:
+            return v  # Если BOOTSTRAP_SERVERS передано явно
+
+        servers = []
+        for i in range(3):
+            server_key = f"SERVER{i}"
+            if server_key in values:
+                servers.append(values[server_key])
+
+        return servers if servers else None
+
+    model_config = SettingsConfigDict(
+        env_file='.env',
+        env_prefix='KAFKA_',
+        extra='ignore',
+    )
+
+
+class ClickhouseConfig(BaseSettings):
+    ANALYTICS_TABLE: str = 'movies.test_analytics'
+    HOST: str = 'clickhouse'
+    PORT: int = 8123
+
+    model_config = SettingsConfigDict(
+        env_file='.env',
+        env_prefix='CLICKHOUSE_',
+        extra='ignore',
+    )
+
+
+kafka_config = KafkaConfig()
+ch_config = ClickhouseConfig()
--- ETL/consumer.py
+++ ETL/consumer.py
@@ -1,22 +1,15 @@
 import asyncio
 import json
-from os import getenv
 
 from aiokafka import AIOKafkaConsumer
 import clickhouse_connect
 
-
-KAFKA_TOPICS = ['my_topic', 'test_topic']
-KAFKA_CONSUMER_GROUP_ID = 'analytics'
-KAFKA_BOOTSTRAP_SERVERS = [getenv('KAFKA_SERVER0'), getenv('KAFKA_SERVER1'), getenv('KAFKA_SERVER2')]
-CLICKHOUSE_ANALYTICS_TABLE = 'movies.test_analytics'
-CLICKHOUSE_HOST = 'clickhouse'
-CLICKHOUSE_PORT = 8123
+from .config import kafka_config, ch_config, Topics
 
 
 async def load_data(data: dict):
-    ch_client = await clickhouse_connect.get_async_client(host=CLICKHOUSE_HOST, port=CLICKHOUSE_PORT)
-    result = await ch_client.insert(CLICKHOUSE_ANALYTICS_TABLE, [list(data.values())], list(data.keys()))
+    ch_client = await clickhouse_connect.get_async_client(host=ch_config.HOST, port=ch_config.PORT)
+    result = await ch_client.insert(ch_config.ANALYTICS_TABLE, [list(data.values())], list(data.keys()))
     print(result.summary)
 
 
@@ -26,9 +19,9 @@ def deserializer(data: bytes) -> dict:
 
 async def consume():
     consumer = AIOKafkaConsumer(
-        *KAFKA_TOPICS,
-        bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS,
-        group_id=KAFKA_CONSUMER_GROUP_ID,
+        *[t.value for t in Topics],
+        bootstrap_servers=kafka_config.BOOTSTRAP_SERVERS,
+        group_id=kafka_config.CONSUMER_GROUP_ID,
         enable_auto_commit=False,
     )
     async with consumer as c:
