1cd645d8000c44b04dff8b326def1ee8590c19a8
Vladislav Vasilev <vyruscd@gmail.com>
2025-05-25 15:02:45 +0400

    chore(ETL): Add credentials to config

--- ETL/config.py
+++ ETL/config.py
@@ -30,6 +30,8 @@ class ClickhouseConfig(BaseSettings):
     ANALYTICS_TABLE: str = 'movies.test_analytics'
     HOST: str = 'clickhouse'
     PORT: int = 8123
+    USER: str
+    PASSWORD: str
 
     model_config = SettingsConfigDict(
         env_file='.env',
