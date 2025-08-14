b3eadbbc775c5c1472f40994ec11bed674164029
Vladislav Vasilev <vyruscd@gmail.com>
2025-08-14 17:54:52 +0400

    (fix): Create logstash keystore optionally

--- infra/ELK/logstash/start.sh
+++ infra/ELK/logstash/start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-bin/logstash-keystore create
-cat /run/secrets/logstash_es_user | bin/logstash-keystore add ES_USER --stdin
-cat /run/secrets/logstash_es_password | bin/logstash-keystore add ES_PW --stdin
+if [ ! -f ~/config/logstash.keystore ]; then
+    bin/logstash-keystore create
+    cat /run/secrets/logstash_es_user | bin/logstash-keystore add ES_USER --stdin
+    cat /run/secrets/logstash_es_password | bin/logstash-keystore add ES_PW --stdin
+fi
 
 exec logstash --config.reload.automatic -f /config/logstash.conf
